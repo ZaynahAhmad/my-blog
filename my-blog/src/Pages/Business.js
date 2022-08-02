053292573cbad8a0c30ab304fb59b5b1327dfadb
ZaynahA <zaynah.ahmad@cdpipelinedevshop.com>
2022-08-02 14:57:56 +0000

    first commit

--- my-blog/src/Pages/Business.js
+++ my-blog/src/Pages/Business.js
@@ -1,17 +1,38 @@
 import React from "react";
-import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
- 
-export default function Business () {
-    return( <>
-   const
-    <Link to='/'> <button href='https://3000-zaynahahmad-myblog-mif836z7pa0.ws-us54.gitpod.io/#'>Home Page</button> </Link>  
-
-    <h1 className='Business'>Business</h1> </>
+import {Button} from "@mui/material";
+import Grid from "@mui/material/Grid";
+import "./Business.css"
 
 
+const paperStyle= {
+    padding: "20px",
+    height: "60vh",
+    width: 480,
+    margin: "20px auto"
+}
 
+
+export default function Business (){
+    return(
+<> <Button href= "https://3000-zaynahahmad-myblog-mif836z7pa0.ws-us54.gitpod.io/"> Home Page </Button>
+        
+        <h1 className= "Title"> Barclays Donates 1 Million to Delaware State University </h1>
+     
+         <Grid align="center">
+         <img src="https://media.cdn.gradconnection.com/BARCLAYS_BANNER__1vWc729.png" alt="Logo" height={400} width={950}/>
+        <div className= "Business-Text"><p> "Cuando se trata de cosméticos, el término "no tóxico" puede comúnmente utilizado es "libre de cinco". Five-free se refiere a los esmaltes que no contienen cinco ingredientes específicos: formaldehído, tolueno formaldehído y alcanfor. También hay marcas que se comercializan como libres de más sustancia de dibutilo, resina de formaldehído y alcanfor. También hay marcas que se comercializan como libres de más sustancias, como 7-free o 10-free.
+"</p></div>
+        <Grid align="center">
+         <img src="https://www.desu.edu/sites/hub/files/styles/inline_1120/public/0_0_b_barclay_dsu_contigent_2400.jpg?itok=LfCJPkRr" alt="Logo" height={500} width={950}/>
+         
+                <div className= "Business-Text"><p> "La Universidad Estatal de Delaware (DSU o Del State) es una universidad pública de investigación de concesión de tierras históricamente negra en Dover, Delaware. DSU también tiene dos campus satélites,uno en Wilmington y otro en Georgetown. La universidad abarca cuatro colegios y una población diversa de estudiantes de pregrado y grado avanzado. La Universidad Estatal de Delaware está clasificada entre" </p></div>
+         
+         <Grid align="center">
+         <img src="https://th.bing.com/th/id/OIP.ekFfVQz8jO1V2-7LCTUB7gHaFm?pid=ImgDet&rs=1" alt="Logo" height={500} width={950}/>
+         
+         </Grid>
+         </Grid>
+         </Grid>
+         </>
     )
-
-    
 }
---
+++ my-blog/src/Pages/testing.js
@@ -0,0 +1,83 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import { Button } from "@mui/material";
+
+function MainFeaturedPost(props) {
+  const paperStyle={
+    padding: "20px",
+    height: "60vh",
+    width: 480,
+    margin: "20px auto"
+}
+  return (
+    <Paper
+      sx={{
+        position: 'relative',
+        backgroundColor: 'grey.800',
+        color: '#fff',
+        mb: 4,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundImage: `url(${post.image})`,
+      }}
+    >
+      {/* Increase the priority of the hero background image */}
+      {<img style={{ display: 'none' }} src={post.image} alt={post.imageText} />}
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          backgroundColor: 'rgba(0,0,0,.3)',
+        }}
+      />
+      <Grid container>
+        <Grid item md={6}>
+          <Box
+            sx={{
+              position: 'relative',
+              p: { xs: 3, md: 6 },
+              pr: { md: 0 },
+            }}
+          >
+            <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+              {post.title}
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              {post.description}
+            </Typography>
+            <Link variant="subtitle1" href="#">
+              {post.linkText}
+            </Link>
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
+
+export default function testing (){
+    return(
+    <>  <Button href= "https://3000-zaynahahmad-myblog-mif836z7pa0.ws-us54.gitpod.io/" > Home Page </Button>
+    <h1>Barclays Donates 1 million to Delaware State University</h1> 
+    </>
+    
+    )
+    }
+MainFeaturedPost.propTypes = {
+  post: PropTypes.shape({
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    imageText: PropTypes.string.isRequired,
+    linkText: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
